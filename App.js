9fe1260d4d09e67291424dfb250181af70b8f91e
Gyanesh Verma <gyaneshverma999@gmail.com>
2024-02-02 00:05:12 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+
+import Pollage from '../Components/Pollage';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Pollage />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ Pollage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pollage = () => {
+  return (
+    <div>Pollage</div>
+  )
+}
+
+export default Pollage
